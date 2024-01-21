118c91113032c0a6d00fce322720ceb1704cbc50
Rares-Hampi <hampimarian1323@gmail.com>
2024-01-21 20:03:39 +0200

    Remove Dockerfile and update server port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 
 import router from "./routes/func";
-
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
@@ -19,6 +16,6 @@ app.use((req, res) => {
   res.status(404).send("Route doesn't exist");
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(5500, () => {
+  console.log(`Server listening on port 5500`);
 });
